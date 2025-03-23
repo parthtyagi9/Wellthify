8ea0889c4aaee2c1bba10facce6cdaf190f28f2a
tyagipa2 <parth.tyagi@mail.utoronto.ca>
2025-03-23 04:29:32 -0400

    final commit project completed

--- clean-genai-hacks/Frontend/genai-hacks/src/App.js
+++ clean-genai-hacks/Frontend/genai-hacks/src/App.js
@@ -1,25 +1,3 @@
-// import './App.css';
-// import Dashboard from './Dashboard.js';
-// import Agent from './Agent.js'
-// import Home from './home.js';
-// import { Routes, Route } from 'react-router-dom';
-// import Choices from './choices.js';
-// import Dietitian from './Dietitian';
-
-// function App() {
-//   return (
-//     // React app
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/choices" element={<Choices />} />
-//       <Route path="/agent/Dietitian" element={<Dietitian />} />
-//       {/* <Route path="/agent/:agentType" element={<Agent />} /> */}
-//     </Routes>
-//   );
-// }
-
-
-// export default App;
 import './App.css';
 import Dashboard from './Dashboard.js';
 import Therapist from './Therapist.js';  // Our 3D therapist scene
